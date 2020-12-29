390ffbaf80bad379d46b6f7397b076d5bcb1e621
ethanswe <ethanaschnitzer@gmail.com>
2020-12-29 13:57:47 -0700

    rough draft of homepage

--- react-app/src/components/HomePage.js
+++ react-app/src/components/HomePage.js
@@ -3,14 +3,40 @@ import { NavLink, useLocation } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import styled from 'styled-components'
 
+const Header = styled.div`
+font-size: 40px;
+display: flex;
+align-items: center;
+justify-content: center;
+margin-top: 90px;
+position: relative;
+z-index: 10;
+`
 
+
+const BackgroundImage = styled.div`
+background-image: url('https://images.pexels.com/photos/1081031/pexels-photo-1081031.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+width: 100vw;
+height: 100vh;
+background-size: cover;
+display: flex;
+align-items: center;
+justify-content: center;
+z-index: -10;
+position: absolute;
+`
 const HomePage = () => {
 
 
 
 
     return (
-        <h2>Test</h2>
+        <>
+        <>
+            <BackgroundImage />
+            <Header>The #1 app for runners and cyclists</Header>
+            </>
+        </>
     )
 }
 
