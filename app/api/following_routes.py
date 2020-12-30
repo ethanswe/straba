544559773f2132077a231c340633641c8557ba32
IvanGent <gentilleivan@gmail.com>
2020-12-29 20:03:32 -0500

    added following routes

--- app/api/following_routes.py
+++ app/api/following_routes.py
@@ -5,6 +5,7 @@ from sqlalchemy.exc import SQLAlchemyError
 
 following_routes = Blueprint('following', __name__)
 
+# GET all followers and followings for a user
 @following_routes.route('/<int:user_id>')
 def following(user_id):
     try:
@@ -14,10 +15,24 @@ def following(user_id):
         followers_dict = [followers.to_dict() for person in followers]
         following_json = jsonify({
             'followers': followers_dict,
-            'following': follows_dict
+            'following': follows_dict,
+            'followersLen': len(followers_dict),
+            'followingLen': len(follows_dict)
         })
         return following_json
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
         print(error)
         return {'errors': ['An error occurred while retrieving the data']}, 500
+
+# POST a new follow
+@following_routes.route('/<int:user_id>', methods=["POST"])
+def post_following(user_id):
+    data = request.json
+    follow = Following(
+        user_id=user_id,
+        followed_user_id=data['userId']
+    )
+    db.session.add(follow)
+    db.session.commit()
+    return 'Done', 201
--- react-app/src/components/Following.js
+++ react-app/src/components/Following.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function FollowingComp() {
-    const [followers, setFollowers] = useState([]);
-    const [following, setFollowing] = useState([]);
+    const [followers, setFollowers] = useState([{message: 'You have no followers.'}]);
+    const [following, setFollowing] = useState([{message: 'You are not following anyone.'}]);
 
     const { userId } = useParams();
     
@@ -15,30 +15,35 @@ function FollowingComp() {
         (async () => {
             const response = await fetch(`/api/following/${userId}`)
             const data = await response.json();
-            console.log(data)
-            // if(data.followers.length > 0) {
-            //     setFollowers(data[followers])
-            // } else {
-            //     setFollowers(['You have no followers.'])
-            // }
-            data.followers.length > 0 ? (
+            if (data.followers.length > 0){
                 setFollowers(data[followers])
-            ) : (
-                setFollowers(['You have no followers.'])
-            )
-            data.following.length > 0 ? (
+            }
+            if (data.following.length > 0) {
                 setFollowing(data[following])
-            ) : (
-                setFollowing(['You are not following anyone.'])
-            )
+            }
         })();
         
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
+    let followContent;
+    let followersContent;
+
+    if (followers[0].message) {
+        followersContent = <div>{followers[0].message}</div>
+    } else {
+        followersContent = <div>{followers.map((person) => <div>{person.first_name} {person.last_name}</div>)}</div>
+    }
+    if (following[0].message) {
+        followContent = <div>{following[0].message}</div>
+    } else {
+        followContent = <div>{following.map((person) => <div>{person.first_name} {person.last_name}</div>)}</div>
+    }
 
     return (
         <>
-        <div>{followers}</div>
-        <div>{following}</div>
+        <div>{followersContent}</div>
+        <div>{followContent}</div>
         <h1>This is a test</h1>
         </>
     )
