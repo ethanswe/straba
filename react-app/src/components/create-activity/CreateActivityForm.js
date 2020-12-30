f00ee2a90121c2f5ab5ee596991660932dd02251
john <jgh2102@gmail.com>
2020-12-30 09:58:28 +0900

    activity feed on main page

--- react-app/src/components/create-activity/CreateActivityForm.js
+++ react-app/src/components/create-activity/CreateActivityForm.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react'
 import { Form, Input, TextArea, Button } from 'semantic-ui-react'
-<<<<<<< HEAD
-import { ActivityFeed } from '../activities-feed/Activities';
-=======
 import styled from 'styled-components'
 
 const BackgroundImg = styled.div`
@@ -33,7 +30,6 @@ z-index: 1;
 margin-top: 150px;
 position: absolute;
 `
->>>>>>> master
 
 export const CreateActivityForm = () => {
     const [title, setTitle] = useState("");
@@ -44,76 +40,6 @@ export const CreateActivityForm = () => {
     const [user_id, setUser_Id] = useState(1);
     return (
         <>
-<<<<<<< HEAD
-        <Form>
-            <Form.Field>
-                <Input 
-                placeholder="Title" 
-                value={title} 
-                onChange={e => setTitle(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <TextArea 
-                placeholder="Describe your activity." 
-                value={description}
-                onChange={(e)=> setDescription(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                   label={{ basic: true, content: 'miles' }}
-                   labelPosition='right'
-                   placeholder='Distance'
-                   value={distance_string}
-                   onChange={(e)=> setDistance(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                   label={{ basic: true, content: 'minutes' }}
-                   labelPosition='right'
-                   placeholder='Time'
-                   value={time_string}
-                   onChange={(e)=> setTime(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input 
-                placeholder="GPX File" 
-                value={gpx_file} 
-                onChange={e => setGPX_file(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Button onClick = {async () => {
-                    const time = parseFloat(time_string).toFixed(2)
-                    const distance = parseFloat(distance_string).toFixed(2)
-                    console.log(time)
-                    console.log(distance)
-                    const activity = {title, description, distance, time, gpx_file, user_id};
-                    console.log(activity)
-                    const res = await fetch(`/api/activities/new/${user_id}`, {
-                        // /api/activities/users/1/activities/new
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(activity)
-                    })
-                    if (res.ok){
-                        console.log('response worked')
-                    } else {
-                        console.log(res)
-                        console.log('POST failed.')
-                    }
-                }}>
-                    Submit
-                </Button>
-            </Form.Field>
-        </Form>
-        <ActivityFeed />
-=======
             <BackgroundImg />
                 <Container>
                     <Form>
@@ -184,7 +110,7 @@ export const CreateActivityForm = () => {
                         </Form.Field>
                     </Form>
             </Container>
->>>>>>> master
+           
         </>
     )
 }
